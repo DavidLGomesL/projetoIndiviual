7c0e6fc178766de69662b6aad4807673fcd5d965
David Luan Gomes <davidluangomeslima@gmail.com>
2022-03-07 12:11:47 -0300

    Adicao da mascara 'Foi adicionada uma mascara no input valor para que os numeros digitados possam receber ponto e virgula.' 'Foi adicionada uma mascara no input valor para que os numeros digitados possam receber ponto e virgula.

--- assets/js/localstorage.js
+++ assets/js/localstorage.js
@@ -78,3 +78,24 @@ function limparDados () {
   minimumFractionDigits: 2,
 });*/
 
+String.prototype.reverse = function(){
+  return this.split('').reverse().join(''); 
+};
+
+function mascaraMoeda(campo,evento){
+  var tecla = (!evento) ? window.event.keyCode : evento.which;
+  var valor  =  campo.value.replace(/[^\d]+/gi,'').reverse();
+  var resultado  = "";
+  var mascara = "##.###.###,##".reverse();
+  for (var x=0, y=0; x<mascara.length && y<valor.length;) {
+    if (mascara.charAt(x) != '#') {
+      resultado += mascara.charAt(x);
+      x++;
+    } else {
+      resultado += valor.charAt(y);
+      y++;
+      x++;
+    }
+  }
+  campo.value = resultado.reverse();
+}
