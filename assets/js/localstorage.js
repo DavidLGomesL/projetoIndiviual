3474b6e82539f9a38da6c4e94ccd7166b4545c97
David Luan Gomes <davidluangomeslima@gmail.com>
2022-03-07 12:04:54 -0300

    Desenvolvimento localStorage No momento esta sendo visto a persistencia dos dados da tabela na tela de cadastro e a inclusao dos mesmos no localStorage.

--- assets/js/localstorage.js
+++ assets/js/localstorage.js
@@ -5,27 +5,24 @@
 
 // Local Storage trabalha com strings, os valores por mais que sejam lidos independete de quais forem todos são transformados em string pelo local storage 
 
-let objStorage = []  // Tendo em mente que o valor do local storage  e dado em string 
+var objStorage = []  // Tendo em mente que o valor do local storage  e dado em string 
+let convertStorage = localStorage.getItem('objStorage');
+if (convertStorage != null) {
+  objStorage = JSON.parse(localStorage.getItem("convertStorage"));
+}
 
-var tabela = document.querySelector('.tabelaGeral')
-let nome = document.getElementById('nameMercadoria')
-let valorNome = nome.value
-let currency = document.querySelector('.btnValor')
-let valor = currency.value
 
-// let convertStorage = localStorage.getItem( JSON.parse('objStorage'));
+
 
 // if (convertStorage)
 
 // let convertStorage = localStorage.getItem( JSON.parse('objStorage'))
 // console.log
 
-if (localStorage.getItem("convertStorage")) {
+/*if (localStorage.getItem("convertStorage")) {
   objStorage = JSON.parse(localStorage.getItem("convertStorage"));
-}
-console.log (objStorage)
+}*/
 
-console.log (typeof objStorage)
 
 // (objStorage != "" ?  ""  : "")
 
@@ -35,34 +32,49 @@ console.log (typeof objStorage)
 
 function lerTabela () {
 // Dom nome e valor dos input
-
+var tabela = document.querySelector('.tabelaGeral')
+let nome = document.querySelector('.btnMercadoria')
+let nomeMercadoria = nome.value
+let currency = document.querySelector('.btnValor')
+let valor = currency.value
+let tipoSelecao = document.getElementsByName('trade')
 
 
 tabela.innerHTML += `<tr>
-<td class="produto">${valorNome}</td> <td class="preco">${valor}</td>
+
+<td class="produto">${tipoSelecao.value == 'compra' ? '-' : '+' }&nbsp;${nomeMercadoria}</td> 
+
+<td class="preco">${valor}</td>
 </tr>`
 
 
-}
+
+
+};
+
+/*function totalTabela () {
+  var totalTable = document.querySelector('.tabelaGeral .tbody')
+
+  totalTable.innerHTML += `<tr> 
+    <td>Total</td>
+    <td>1234</td>
+  </tr>`
+};*/
 
 function limparDados () {
   let produto = document.querySelector('.produto')
   let preco = document.querySelector('.preco')
   produto.remove()
   preco.remove()
-}
+  //localStorage.clear();
+  //localStorage.setItem('objStorage', JSON.stringify(objStorage))
+  objStorage = [];
+};
 
+//Formatar para moeda local
+/*const currencyFormat = new Intl.NumberFormat("pt-br", {
+  style: "currency",
+  currency: "BRL",
+  minimumFractionDigits: 2,
+});*/
 
-
-// Send to local stororage enviar local storage:
-
-let sendStorage = [
-  {
-    convertStorage: valorNome,
-    convertStorage: valor,
-  },
-
-  
-]
-
-console.log(sendStorage)
