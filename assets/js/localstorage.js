deb36d1e273be9176c622cb8899d4e279e883387
David Luan Gomes <davidluangomeslima@gmail.com>
2022-03-28 17:03:58 -0300

    Correçoes da mascaraMoeda, calculo e tabela Foi realizada a correçao da mascaraMoeda, foi ajustada a subtraçao dos valores (ainda esta havendo um bug no resultado) e falta corrigir a duplicidade da tabela.

--- assets/js/localstorage.js
+++ assets/js/localstorage.js
@@ -14,131 +14,122 @@ if (convertStorage != null) {
 
 desenhaTabela()
 function desenhaTabela () { //Mantém a tabela
-  for (transaction of objStorage) {
-    document.querySelector('.tabelaGeral').innerHTML += `
-  <tr>
-
-    <td class="produto">${transaction.tipo}&nbsp;${transaction.nome}</td> 
-
-    <td class="preco">${transaction.valor}</td>
-  </tr>
-  `
-
-  let tdsValores = document.querySelectorAll('.preco');
-
+  
   let total = 0
 
-  for (let i = 0; i < tdsValores.length; i++) {
-    let valor = parseFloat(tdsValores[i].textContent)
-    total += valor;
-  }
+  objStorage.forEach((item) => {
+    total += parseFloat(item.valor.replaceAll('.','').replaceAll(',','').replace(/([0-9][0-9])$/g, '.$1')) * parseFloat(item.tipo+'1')
+  })
 
-  document.querySelector('.tabelaGeral .foot').innerHTML += `
-  <tr class="tabTotal">
+    for (transaction of objStorage) {
+        document.querySelector('.tabelaGeral').innerHTML += `
+      <tr>
 
-    <td>Total</td> 
+        <td class="produto">${transaction.tipo}&nbsp;${transaction.nome}</td> 
 
-    <td class="qtdTotal">${total}</td>
-  </tr>
-  `
+        <td class="preco">${transaction.valor}</td>
+      </tr>
 
-  if (objStorage.length > 0) {
-    document.querySelector('.tabelaGeral tfoot').innerHTML += `
-    <tr class="lucroPrejuizo">
-      <td></td>
-      <td class="status">${Math.sign(total) > 0 ? "[LUCRO]" : "[PREJUÍZO]"}</td>
-    </tr>
-    `
-  }
-  };
+      <tr class="tabTotal">
 
+          <td>Total</td> 
 
+          <td class="qtdTotal">${total}</td>
+      </tr>
+      <!--Mostra status de Lucro ou Prejuízo-->
+      <tr class="lucroPrejuizo">
+        <td></td>
+        <td class="status">${Math.sign(total) > 0 ? "[LUCRO]" : "[PREJUÍZO]"}</td>
+      </tr>
+      `
+      
+      console.log(total)
+    };
+
+  
 }
 
 
 
 function lerTabela () {
-// Dom nome e valor dos input
-var tabela = document.querySelector('.tabelaGeral')
-let nome = document.querySelector('.btnMercadoria')
-let nomeMercadoria = nome.value
-let currency = document.querySelector('.btnValor')
-let valor = currency.value
-let tipoSelecao = document.getElementsByName('trade')
+  // Dom nome e valor dos input
+  let nome = document.querySelector('.btnMercadoria')
+  let nomeMercadoria = nome.value
+  let currency = document.querySelector('.btnValor')
+  let valor = currency.value
+  let tipoSelecao = document.querySelector('#transacao').value == 'compra' ? '-' : '+'
+  let total = 0
 
+ 
+  document.querySelector('.tabelaGeral').innerHTML += `
+      <tr>
 
-tabela.innerHTML += `
-  <tr class="dinamic-content">
+        <td class="produto">${tipoSelecao}&nbsp;${nomeMercadoria}</td> 
 
-    <td class="produto">${tipoSelecao.value == "compra" ? "+" : "-"}&nbsp;${nomeMercadoria}</td> 
+        <td class="preco">${valor}</td>
+      </tr>
 
-    <td class="preco">${valor}</td>
-  </tr>`
+      <tr class="tabTotal">
+
+          <td>Total</td> 
+
+          <td class="qtdTotal">${total}</td>
+      </tr>
+      <!--Mostra status de Lucro ou Prejuízo-->
+      <tr class="lucroPrejuizo">
+        <td></td>
+        <td class="status">${Math.sign(total) > 0 ? "[LUCRO]" : "[PREJUÍZO]"}</td>
+      </tr>
+      `
 
   objStorage.push({
     nome: nomeMercadoria,
     valor: valor,
-    tipo: tipoSelecao.value == "compra" ? "-" : "+"
+    tipo: tipoSelecao
   })
 
 
   localStorage.setItem('objStorage', JSON.stringify(objStorage))
-  
-  //Calcula o valor da tabela, mostra total e status.
-  let tdsValores = document.querySelectorAll('.preco');
 
-  let total = 0
+  objStorage.forEach((item) => {
+    total += parseFloat(item.valor.replaceAll('.','').replaceAll(',','').replace(/([0-9][0-9])$/g, '.$1')) * parseFloat(item.tipo+'1')
+  })
 
-  for (let i = 0; i < tdsValores.length; i++) {
-    let valor = parseFloat(tdsValores[i].textContent)
-    total += valor;
-  }
-
-  document.querySelector('.tabelaGeral .foot').innerHTML += `
-  <tr class="tabTotal">
-
-    <td>Total</td> 
-
-    <td class="qtdTotal">${total}</td>
-  </tr>
-  `
-
-  if (objStorage.length > 0) {
-    document.querySelector('.tabelaGeral tfoot').innerHTML += `
-    <tr class="lucroPrejuizo">
-      <td></td>
-      <td class="status">${Math.sign(total) > 0 ? "[LUCRO]" : "[PREJUÍZO]"}</td>
-    </tr>
-    `
-  }
-
-  console.log(total);
+  total
 
 };
 
 
 function limparDados () { //Limpa a tabela
-  let produtos = document.querySelectorAll('.produto')
-  let precos = document.querySelectorAll('.preco')
-  let totals = document.querySelectorAll('.tabTotal')
-  let lP = document.querySelectorAll('.lucroPrejuizo')
-  
-  produtos.forEach((element) => {
-    element.remove();
-  })
-  precos.forEach((element) => {
-    element.remove();
-  })
-  totals.forEach((element) => {
-    element.remove();
-  })
-  lP.forEach((element) => {
-    element.remove();
-  })
 
-  objStorage = [];
-  localStorage.setItem('objStorage', JSON.stringify(objStorage))
-  alert('Os dados serão removidos!')
+    let produtos = document.querySelectorAll('.produto')
+    let precos = document.querySelectorAll('.preco')
+    let totals = document.querySelectorAll('.tabTotal')
+    let lP = document.querySelectorAll('.lucroPrejuizo')
+
+    if (confirm("Deseja remover os dados da tabela?")) {
+
+      produtos.forEach((element) => {
+        element.remove();
+      })
+      precos.forEach((element) => {
+        element.remove();
+      })
+      totals.forEach((element) => {
+        element.remove();
+      })
+      lP.forEach((element) => {
+        element.remove();
+      })
+  
+      objStorage = [];
+      localStorage.setItem('objStorage', JSON.stringify(objStorage))
+
+      return true;
+    } else {
+      return false;
+    }
+    
 };
 
 //Mascara da tabela
@@ -150,7 +141,7 @@ function mascaraMoeda(campo,evento){
   var tecla = (!evento) ? window.event.keyCode : evento.which;
   var valor  =  campo.value.replace(/[^\d]+/gi,'').reverse();
   var resultado  = "";
-  var mascara = "##.###.##".reverse();
+  var mascara = "##.###.###,##".reverse();
   for (var x=0, y=0; x<mascara.length && y<valor.length;) {
     if (mascara.charAt(x) != '#') {
       resultado += mascara.charAt(x);
